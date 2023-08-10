50600cda6e3086ffff5b75e6773b1dbb62426906
Iuri Volnistem <cit.analista4@adm.unitoledo.br>
2023-08-10 16:52:57 -0300

    adicionado Router ao projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
-import { ThemeProvider } from "styled-components";
-import { defaultTheme } from "./styles/themes/default";
-import { GlobalStyle } from "./styles/global";
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './Router'
+
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/global'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <h1>Hello world</h1>
-
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   )
